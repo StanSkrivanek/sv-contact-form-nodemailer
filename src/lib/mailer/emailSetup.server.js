e2af2b0f70d084e3b79677adba85dbaf8691e156
Stan Skrivanek <s.skrivanek@icloud.com>
2023-09-03 00:48:12 +0100

    ADD: nodemailer -works

---
+++ src/lib/mailer/emailSetup.server.js
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { GOOGLE_EMAIL, GOOGLE_EMAIL_PASSWORD } from '$env/static/private';
+
+let transporter = nodemailer.createTransport({
+	host: 'smtp.gmail.com',
+	port: 465,
+	secure: true,
+	auth: {
+		user: GOOGLE_EMAIL,
+		pass: GOOGLE_EMAIL_PASSWORD
+	}
+});
+
+transporter.verify((error) => {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Server is ready to take messages');
+	}
+});
+
+export default transporter;
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,7 @@
+import { GOOGLE_EMAIL } from '$env/static/private';
+import transporter from '$lib/mailer/emailSetup.server';
 import { contactEmailSchema } from '$lib/types';
+
 /** @type {import('./$types').Actions} */
 export const actions = {
 	default: async ({ request }) => {
@@ -14,6 +17,29 @@ export const actions = {
 		try {
 			// try validate data
 			contactEmailSchema.parse(contactEmailData);
+
+			// if valid send email
+			const emailMessage = {
+				from: `"${contactEmailData.name}" <${contactEmailData.email}>`,
+				to: GOOGLE_EMAIL,
+				subject: `New message from ${contactEmailData.name}`,
+				text: `Name: ${contactEmailData.name}\nEmail: ${contactEmailData.email}\nService: ${contactEmailData.service}\nBudget: ${contactEmailData.budget}\nMessage: ${contactEmailData.message}`
+			};
+			// @ts-ignore
+			const sendEmail = async (emailMessage) => {
+				await new Promise((resolve, reject) => {
+					transporter.sendMail(emailMessage, (err, info) => {
+						if (err) {
+							console.log(err);
+							reject(err);
+						} else {
+							console.log(info);
+							resolve(info);
+						}
+					});
+				});
+			};
+			await sendEmail(emailMessage);
 		} catch (err) {
 			// if not valid return errors
 			// @ts-ignore
@@ -25,10 +51,10 @@ export const actions = {
 				data: rest
 			};
 		}
-		console.log(contactEmailData);
+
 		return {
 			// if data pass zod validatin -> return success
-			success: true,
+			success: true
 			// after successful submission I'm getting error
 			// Unhandled Promise Rejection: TypeError: undefined is not an object (evaluating 'ctx[0]?.data.name')
 			// prevent [ctx].data from being undefined
